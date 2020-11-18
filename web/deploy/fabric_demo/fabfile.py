d5c1b3d9b4f5d070f5f7190b8677805e8572ff28
ikaroskun <merle.liukun@gmail.com>
2020-11-18 20:14:30 +0800

    refactor(deploy): :art: fabfile

--- web/deploy/fabric_demo/fabfile.py
+++ web/deploy/fabric_demo/fabfile.py
@@ -4,9 +4,6 @@
     by http://www.fabfile.org/index.html.
     对于小型 Web 项目的部署优化
     默认部署的是 dev 环境下的 dev 分支
-    说明：
-        1. 分为两个部署环境(可以跨机器), dev 与 prod
-        2. 不同环境下使用不同的 虚拟环境 (可共用)
 """
 
 import os
@@ -15,80 +12,72 @@ from fabric import Connection, task
 from invoke import Exit
 
 host_string = 'ubuntu@127.0.0.1'
+home_dir = "path/to/project"
+python_env = "path/to/env"
+ini_file = "path/to/uwsgi.ini"
+pid_file = 'path/to/uwsgi.pid'
+
+if not os.path.exists(home_dir):
+    raise Exit(f"Aborting for: project path {home_dir} is not exists!")
+
+if not os.path.exists(python_env):
+    raise Exit(f"Aborting for: env path {python_env} is not exists!")
 
 
 @task
-def deploy(ctx, env='dev', branch=None):
+def deploy(ctx, branch=None):
     """
     :param ctx: default context for invoke.
     :param env: the environment for project run.
     :param branch: target branch.
     :return:
     """
-    home_dir = '~/projects/{}/project_name'.format(env)
-    python_env = '~/.pyenv/versions/project_{}_env'.format(env)
-    if not os.path.exists(home_dir):
-        raise Exit("Aborting for: file path is not exists! Check the target param.")
 
     if branch is None:
-        branch = 'dev'
-
-    if env == 'prod' and branch != 'master':
-        raise Exit("Aborting for: Production must be master branch.")
+        branch = "dev"
 
     with Connection(host_string) as conn:
         print("Now Begin to deploy")
-        conn.local("cd {}".format(home_dir))
+        conn.local("cd {home_dir}")
 
         # stop older worker.
-        pid_file = 'var/pids/uwsgi.pid'
-        conn.local("kill -9 `cat {}`".format(pid_file))
-        print("You have stop uwsgi workers on env: {}".format(env))
+        conn.local(f"kill -9 `cat {pid_file}`")
+        print(f"You have stop uwsgi workers on env: {env}")
 
         # git op
         print("Now fetching latest code.")
         conn.local("git fetch --all -p")
-        conn.local("git reset --hard origin/{}".format(branch))
+        conn.local(f"git reset --hard origin/{branch}")
         # run
-        conn.local("{}/bin/uwsgi myapp.ini ".format(python_env))
-        print("You have deployed latest code on env: {0}, Used Branch is: {1}".format(env, branch))
+        conn.local(f"{python_env}/bin/uwsgi {ini_file}")
+        print(f"You have deployed latest code used Branch is: {ranch}")
 
 
 @task
-def stop(ctx, env='dev'):
+def stop(ctx):
     """
     :param ctx: default context for invoke.
-    :param env: the environment for project run.
     :return:
     """
-    home_dir = '~/projects/{}/project_name'.format(env)
-    if not os.path.exists(home_dir):
-        raise Exit("Aborting for: file path is not exists! Check the target param.")
 
     with Connection(host_string) as conn:
         print("Now Begin to stop uwsgi workers.")
-        conn.local("cd {}".format(home_dir))
+        conn.local(f"cd {home_dir}")
         # kill current env uwsgi process.
-        pid_file = 'var/pids/uwsgi.pid'
-        conn.local("kill -9 `cat {}`".format(pid_file))
-        print("You have stop uwsgi workers on env: {}".format(env))
+        conn.local(f"kill -9 `cat {pid_file}`")
+        print("You have stop uwsgi workers.)
 
 
 @task
-def start(ctx, env='dev'):
+def start(ctx):
     """
     不拉取代码 直接启动
     :param ctx: default context for invoke.
-    :param env: the environment for project run.
     :return:
     """
-    home_dir = '~/projects/{}/project_name'.format(env)
-    python_env = '~/.pyenv/versions/project_{}_env'.format(env)
-    if not os.path.exists(home_dir):
-        raise Exit("Aborting for: file path is not exists! Check the target param.")
     with Connection(host_string) as conn:
-        conn.local("{}/bin/uwsgi myapp.ini".format(python_env))
-        print("You have start uwsgi workers on env: {0}".format(env))
+        conn.local(f"{python_env}/bin/uwsgi {ini_file}")
+        print("You have start uwsgi workers")
 
 
 @task
@@ -96,9 +85,8 @@ def restart(ctx, env='dev'):
     """
     不拉取代码 直接重新启动
     :param ctx: default context for invoke.
-    :param env: the environment for project run.
     :return:
     """
-    stop(ctx, env)
-    start(ctx, env)
+    stop(ctx)
+    start(ctx)
     print("Restart Finish!")
